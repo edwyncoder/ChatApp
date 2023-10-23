cbcb5ee45399916bd021c362e0e7a9304a31784d
edwyncoder <edwyn.t@flyerssoft.com>
2023-10-23 17:20:10 +0530

    added rooms for making chat with perticular person

--- index.js
+++ index.js
@@ -7,18 +7,33 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
+    // console.log('a user connected', socket.id);
+
+    socket.on('join_room', (data) => {
+        console.log("joining a room", data.roomid)
+        socket.join(data.roomid);
+    });
 
     socket.on('msg_send', (data) => {
         console.log(data);
-        io.emit('msg_rcvd', data);
+        // io.emit('msg_rcvd', data);
         // socket.emit('msg_rcvd', data)
         // socket.broadcast.emit('msg_rcvd', data)
+
+        io.to(data.roomid).emit('msg_rcvd', data);
     });
 });
 
 app.use('/', express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+app.get('/chat/:roomid', async (req, res) => {
+   
+    res.render('index', {
+        name: 'Sanket',
+        id: req.params.roomid,
+    });
+});
 
 server.listen(3000, () => {
     console.log('Server started');
