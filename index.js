8e65acbce91f96bc4eb4edea206414a237fa244a
edwyncoder <edwyn.t@flyerssoft.com>
2023-10-23 17:33:49 +0530

    added the logic to make chats persistant

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const socketio = require('socket.io');
 const connect = require('./config/database-config');
 
+const Chat = require('./models/chat');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -15,12 +17,18 @@ io.on('connection', (socket) => {
         socket.join(data.roomid);
     });
 
-    socket.on('msg_send', (data) => {
+    socket.on('msg_send', async(data) => {
         console.log(data);
         // io.emit('msg_rcvd', data);
         // socket.emit('msg_rcvd', data)
         // socket.broadcast.emit('msg_rcvd', data)
 
+        console.log(data);
+        const chat = await Chat.create({
+            roomId: data.roomid,
+            user: data.username,
+            content: data.msg
+        });
         io.to(data.roomid).emit('msg_rcvd', data);
     });
 });
@@ -29,10 +37,14 @@ app.use('/', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 app.get('/chat/:roomid', async (req, res) => {
-   
+    const chats = await Chat.find({
+        roomId: req.params.roomid
+    }).select('content user');
+    console.log(chats);
     res.render('index', {
         name: 'Sanket',
         id: req.params.roomid,
+        chats: chats
     });
 });
 
